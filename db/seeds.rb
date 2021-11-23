86b582aa839365669a739195f290ee2d3c40dadc
gedunlap <g.e.dunlap90@gmail.com>
2021-11-23 14:53:42 -0500

    seed and cors

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Entry.create({category:"Beast", name:"Wolf", sword:"Steel", spell:"Igni"})
+Entry.create({category:"Draconid", name:"Basilisk", sword:"Silver", spell:"Aard"})
+Entry.create({category:"Elemental", name:"Golem", sword:"Silver", spell:"Quen"})
+Entry.create({category:"Hybrid", name:"Griffin", sword:"Silver", spell:"Aard"})
+Entry.create({category:"Specter", name:"Plague Maiden", sword:"Silver", spell:"Yrden"})
+Entry.create({category:"Human", name:"Various", sword:"Steel", spell:"Igni"})
+
+p Entry.all
